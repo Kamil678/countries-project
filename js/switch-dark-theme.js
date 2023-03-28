e171cbd4d803d0fc1e8a80b453dd5b2d54eb24ae
Kamil678 <kamilpigulak@interia.pl>
2023-03-28 14:28:52 +0200

    add dark theme

--- js/switch-dark-theme.js
+++ js/switch-dark-theme.js
@@ -1,4 +1,17 @@
 const darkThemeBtn = document.querySelector(".dark-theme-button");
+let theme = localStorage.getItem("theme") || "light";
+
 darkThemeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    theme = "light";
+    document.body.classList.remove("dark");
+  } else {
+    theme = "dark";
+    document.body.classList.add("dark");
+  }
+  localStorage.setItem("theme", theme);
 });
+
+if (theme === "dark") {
+  document.body.classList.add("dark");
+}
