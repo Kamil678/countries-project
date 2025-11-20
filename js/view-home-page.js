a5ace285274f86f4b0c8a65e0a7a987ead6cc9aa
Kamil Pigulak <kamil.pigulak@mediaflex.pl>
2025-11-20 19:48:06 +0100

    fix: get all countries from API

--- js/view-home-page.js
+++ js/view-home-page.js
@@ -4,7 +4,7 @@ import {
 } from "./functions-DOM.js";
 
 export const renderViewHomePage = () => {
-  const API_URL_ALL = "https://restcountries.com/v3.1/all";
+  const API_URL_ALL = "https://restcountries.com/v3.1/all?fields=capital,population,name,cca3,region,flags";
   let countries;
   let query = "";
   let currentRegion = "";
@@ -17,12 +17,13 @@ export const renderViewHomePage = () => {
           capital: country.capital && country.capital[0],
           population: country.population.toLocaleString(),
           name: country.name.common,
-          code: country.cioc,
+          code: country.cca3,
           region: country.region,
           flagUrl: country.flags.png,
           flagAlt: country.flags.alt,
         };
       });
+      console.log(countries);
       renderCountriesList(countries);
     });
 
